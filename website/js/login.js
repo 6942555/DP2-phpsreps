2a925d01e2ab4d87be156024e621899663514742
Brayden Di Luca <Brayden Di Luca>
2017-03-30 18:37:55 +1100

    Added website files and created basic login page

---
+++ website/js/login.js
@@ -0,0 +1,21 @@
+var app = angular.module("phpsreps", []);
+app.controller ("myCtrl", function ($scope){
+  /*code here*/
+
+  $scope.login = function(){
+    if($scope.uName == 'admin' && $scope.pWord == 'password'){
+      window.location.href = "home.html";
+    }else{
+      $scope.errMsg = "Wrong username or password!";
+    }
+  }
+
+  $scope.delete = function(item){
+    var i = $scope.posts.indexOf(item);
+    $scope.posts.splice(i, 1);
+    $scope.numPosts--;
+  }
+
+}
+);
+
